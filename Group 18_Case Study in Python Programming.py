a3783b6941c7453c0c4f139062534353a59cdbb9
Ratu Syahirah Khairunnisa <135084798+queenxhr@users.noreply.github.com>
2024-06-25 21:33:27 +0700

    initial commit

---
+++ Group 18_Case Study in Python Programming.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # First Case Study : Miles Per Gallon
+
+# In[ ]:
+
+
+
+
+
+# # Second Case Study : A Game of Chance - Crap
+
+# In[ ]:
+
+
+
+
+
+# # Third Case Study : COVID 19 Infection Statistic
+
+# In[1]:
+
+
+import statistics
+
+
+# In[2]:
+
+
+# Data on the number of newly infected patients per day
+data = [174, 335, 278, 214, 422, 513, 737, 672, 489, 412, 1301, 1105, 1123, 1376, 1502, 894, 665, 1704, 1656, 1342]
+
+
+# In[3]:
+
+
+minimum = min(data)
+maximum = max(data)
+range = maximum - minimum
+mean = statistics.mean(data)
+median = statistics.median(data)
+variance = statistics.variance(data)
+standard_deviation = statistics.stdev(data)
+
+
+# In[4]:
+
+
+# Output
+print(f"Minimum: {minimum}")
+print(f"Maximum: {maximum}")
+print(f"Range: {range}")
+print(f"Mean: {mean}")
+print(f"Median: {median}")
+print(f"Variance: {variance}")
+print(f"Standarad Deviation: {standard_deviation}")
+
